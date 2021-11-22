a7419f026607781f311f2f4e3e349ac4b5e89078
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-11-22 18:24:13 -0300

    Commit 2

---
+++ src/main/java/Cuaderno_Universitario/Hoja.java
@@ -0,0 +1,14 @@
+package Cuaderno_Universitario;
+
+public class Hoja {
+	private boolean estado;
+	public Hoja Cuaderno;
+
+	public boolean getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(boolean aEstado) {
+		this.estado = aEstado;
+	}
+}
