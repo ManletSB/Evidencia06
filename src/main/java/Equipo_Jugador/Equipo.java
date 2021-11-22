4358bb1ed3b499e9b81df89675f0a851e2564a27
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-11-22 18:44:33 -0300

    Commit 4

---
+++ src/main/java/Equipo_Jugador/Equipo.java
@@ -0,0 +1,15 @@
+package Equipo_Jugador;
+
+public class Equipo {
+	private int cantidadJugadores;
+	public Capitan Jugador;
+	public Capitan unnamed_Capitan_;
+
+	public int getCantidadJugadores() {
+		return this.cantidadJugadores;
+	}
+
+	public void setCantidadJugadores(int aCantidadJugadores) {
+		this.cantidadJugadores = aCantidadJugadores;
+	}
+}
---
+++ src/main/java/Equipo_Jugador/Jugador.java
@@ -0,0 +1,14 @@
+package Equipo_Jugador;
+
+public class Jugador {
+	private String equipo;
+	public Jugador Equipo;
+
+	public String getEquipo() {
+		return this.equipo;
+	}
+
+	public void setEquipo(String aEquipo) {
+		this.equipo = aEquipo;
+	}
+}
