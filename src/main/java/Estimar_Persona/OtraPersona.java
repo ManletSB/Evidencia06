ad5ec951b695de2823e2b35a75d0a449f52bd063
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-11-22 18:30:55 -0300

    Commit 3

---
+++ src/main/java/Estimar_Persona/OtraPersona.java
@@ -0,0 +1,14 @@
+package Estimar_Persona;
+
+public class OtraPersona {
+	private boolean amigo;
+	public Persona unnamed_Persona_;
+
+	public boolean getAmigo() {
+		return this.amigo;
+	}
+
+	public void setAmigo(boolean aAmigo) {
+		this.amigo = aAmigo;
+	}
+}
---
+++ src/main/java/Estimar_Persona/Persona.java
@@ -0,0 +1,13 @@
+package Estimar_Persona;
+
+public class Persona {
+	public OtraPersona Persona;
+
+	public void estimar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean considerarAmigo() {
+		throw new UnsupportedOperationException();
+	}
+}
