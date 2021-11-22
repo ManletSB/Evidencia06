fdd43dbf59cfc59fe48f670a62f31934126232a9
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-11-22 18:03:41 -0300

    Commit 1

---
+++ src/main/java/Package/Bus.java
@@ -0,0 +1,15 @@
+package Package;
+
+public class Bus {
+	private int antiguedad;
+	public Empresa unnamed_Empresa_;
+	public Comprador _..._;
+
+	public int getAntiguedad() {
+		return this.antiguedad;
+	}
+
+	public void setAntiguedad(int aAntiguedad) {
+		this.antiguedad = aAntiguedad;
+	}
+}
---
+++ src/main/java/Package/Empresa.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Empresa {
+	public Bus _2..._;
+
+	public void comprarBus() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void venderBus() {
+		throw new UnsupportedOperationException();
+	}
+}
